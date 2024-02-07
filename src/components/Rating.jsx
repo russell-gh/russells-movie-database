acf42ba25992fe4c1f97bbe12352233d7f77648c
Your Name <you@example.com>
2024-02-07 20:47:33 +0000

    added rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { FaRegStar } from "react-icons/fa";
+import { FaRegStarHalf } from "react-icons/fa6";
 
 class Rating extends Component {
   render() {
@@ -7,13 +8,28 @@ class Rating extends Component {
 
     console.log(Math.floor(voteAverage), new Array(Math.floor(voteAverage)));
 
+    //create an array where its length matches the could of votes
+    const count = [];
+    for (let i = 0; i < voteAverage; i++) {
+      count.push("");
+    }
+
     return (
       <>
         <p>Total votes: {voteCount}</p>
         <p>Rating: {voteAverage} </p>
-        {new Array(Math.floor(voteAverage)).fill("").map(() => {
-          return <FaRegStar />;
+        {count.map(() => {
+          return (
+            <span className="star">
+              <FaRegStar />
+            </span>
+          );
         })}
+        {voteAverage !== Math.round(voteAverage) && (
+          <span className="star">
+            <FaRegStarHalf />
+          </span>
+        )}
       </>
     );
   }
